ca42eebd08199ce9380302ae8a497145a7d8902c
Happyllama25 <fabsese@outlook.com>
2025-06-03 04:00:11 -0400

    Update save_replay_buffer.py

--- save_replay_buffer.py
+++ save_replay_buffer.py
@@ -14,11 +14,20 @@
 
 import obsws_python as obs
 
-cl = obs.ReqClient(host='localhost', port=4455, password='generate-your-password', timeout=3)
-rp = cl.get_replay_buffer_status()
-active = rp.output_active
+cl = obs.ReqClient(host='localhost', port=4455, password='EZBQzHhaaQTOPDTx', timeout=3)
 
-if not active:
-    cl.start_replay_buffer()
-else:
-    cl.save_replay_buffer()
+# Uncomment the line below and replace your password if you've enabled authentication for your websocket
+# (and comment the above cl line)
+# cl = obs.ReqClient(host='localhost', port=4455, password='password-here', timeout=3)
+
+rp = cl.get_replay_buffer_status()
+
+# active = rp.output_active
+
+cl.save_replay_buffer()
+
+
+# if not active:
+#     cl.start_replay_buffer()
+# else:
+#     cl.save_replay_buffer()
